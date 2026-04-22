72b7d7550f5dfe7a2303cd32a6af60796be98f7e
emiliano <emcon84@gmail.com>
2026-04-22 16:04:25 -0300

    fix: delete proxy routes

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,6 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/mi-perfil(.*)",
-  "/profesional/nuevo(.*)",
   "/profesional/editar(.*)",
 ]);
 
